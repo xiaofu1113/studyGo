5971e79d590d14fe5789fa8b091b83a59ac2caf3
fuxf <121933215@qq.com>
2018-01-16 20:50:02 +0800

    指针数组的初始化

--- 1/main.go
+++ 1/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
+func main() {
 	fmt.Println("我来了，golang")
+
+	/*var i interface{}
+	i = 4
+	fmt.Println(i, reflect.TypeOf(i))
+	i = "abc"
+	fmt.Println(i, reflect.TypeOf(i))*/
+
+	var arr [3]*string
+	arr1 := [3]*string{new(string), new(string), new(string)}
+	*arr1[0] = "red0"
+	*arr1[1] = "red1"
+	*arr1[2] = "red2"
+	arr = arr1
+	fmt.Println(arr)
+	var temp people
+	fmt.Println(temp.getName())
+
+}
+
+type people struct {
+	name string
+	age  int
+	aAge *int
+}
+
+func (peo *people) getName() string {
+	return peo.name
 }
